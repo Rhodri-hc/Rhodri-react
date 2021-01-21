e6ab40526894fd003fe92973d8cf5fd69800c6c6
Rhodri-hc <13622450494@163.com>
2021-01-21 23:46:31 +0800

    顶部图片和hi故障风

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -3,9 +3,13 @@ import Dialog from './component/dialog/index';
 import './about.css'
 
 class About extends Component {
-    
     render() {
         return(
+          <div className='swapper'>
+            <div className='swapper-content'>
+                <img className='about-content__swapper-img' src='../../assets/images/anime-sky.jpg' alt='anime-sky'/>
+                <p className='about-content__swapper-hi' data-text='HI, RHODRI!'>HI, RHODRI!</p>
+            </div>
             <div className='about-content'>
                 <h3 className='about-content__title'>關於</h3>
                 <center>茫茫人海 <span className='about-content__meet'>遇見你</span> 真好</center>
@@ -14,6 +18,8 @@ class About extends Component {
                     <i className='about-content__time'>Last updated in Jan. 20, 2021</i>
                 </center>
             </div>
+          </div>
+            
         )
     }
 }
