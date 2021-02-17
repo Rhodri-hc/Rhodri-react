e30adcaefb902ebfb4f9266c8017bd70ce3b3088
Rhodri-hc <13622450494@163.com>
2021-02-17 22:50:21 +0800

    使用css in js styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React, { Component, Fragment } from "react";
 import About from './views/about/About';
 import './App.css';
+import { Globalstyle } from './style'
 
-function App() {
-  return (
-    <div>
-      <div className='about'>
-        <About  />
-      </div>
-      <footer className='footer'>
-        <center className='footer-creater'>
-          <span className='footer-creater-left'>Crafted with</span>
-          <i className='footer-heart'></i> 
-          <span className='footer-creater-right'>by Rhodri</span>
-        </center>
-      </footer>
-    </div>
-    
-  );
-}
+class App extends  Component {
+  render(){
+    return (
+      <Fragment>
+        <Globalstyle />
+          <div className='about'>
+            <About  />
+          </div>
+          <footer className='footer'>
+            <center className='footer-creater'>
+              <span className='footer-creater-left'>Crafted with</span>
+              <i className='footer-heart'></i> 
+              <span className='footer-creater-right'>by Rhodri</span>
+            </center>
+          </footer>
+      </Fragment>
+    )
+  }
+} 
 
 export default App;
