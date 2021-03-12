9789f8de3bdf00e332e65697cbe4893b2269c1b7
Rhodri-hc <13622450494@163.com>
2021-03-12 19:32:21 +0800

    新增文章块

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -7,6 +7,10 @@ class Home extends Component {
             <div className="relative pt-14">
                 <div className="w-11/12 sm:w-11/12 md:w-9/12 lg:w-8/12 xl:w-7/12 my-0 mx-auto">
                    <Title>文章</Title>
+
+                   <div className="flex flex-col items-center mt-16">
+                       <div className="rounded-lg h-56 w-9/12 shadow-lg  cursor-pointer transform hover:-translate-y-1 hover:scale-105 transition duration-700 ease-in-out"></div>
+                   </div>
                 </div>
             </div>
         )
